cda491cf8a2934e8a246f411f67e10f7329b84a1
Viktor Kovalenko <vik85kov@gmail.com>
2022-02-16 12:30:49 +0200

    add index.html

--- landingpagewebside/settings.py
+++ landingpagewebside/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'landingpagewebside.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
